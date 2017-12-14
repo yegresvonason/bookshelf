e71d37e6d2bbbd5fa25d0f8a7c5d7379ffcbf1c4
yegresvonason <yegresvonason@gmail.com>
2017-12-14 22:19:33 +0400

    Верстка

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('#addButton').click(function(){
+        $('#modal-window').fadeIn(600);
+    });
+    
+    $('#cancelButton').click(function(){
+        $('#modal-window').fadeOut(600);
+    });
+});
